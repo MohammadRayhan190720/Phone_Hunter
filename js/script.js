7fa4d0a0bec72c15579bcd8b56c0a6edaa841464
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-08 19:39:20 +0600

    added functionally

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const loadAllPhones = () =>{
-  console.log("3 seconds gone")
+const loadAllPhones = async() =>{
+
+  document.getElementById("spinner").style.display = "none";
+
+  //fetch data
+  // fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+  // .then(res => res.json())
+  // .then(data => console.log(data.data))
+
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+  const data = await res.json();
+  console.log(data.data);
 }
 
 
@@ -7,6 +17,7 @@ const loadAllPhones = () =>{
 
 
 const handleSearch = () => {
+  document.getElementById("spinner").style.display = "block";
 
   setTimeout(() => {
     loadAllPhones();
