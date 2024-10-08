ff82841f358547561bcd97b187fda8214b82f2e2
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-09 01:06:57 +0600

    added functionally

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const loadAllPhones = async(status,searchText) =>{
   // .then(data => console.log(data.data))
 
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/phones?search=iphone`
+    `https://openapi.programming-hero.com/api/phones?search=${searchText?searchText: "iphone"}`
   );
   const data = await res.json();
   console.log(data)
@@ -27,20 +27,22 @@ const displayAllPhones = (phones) =>{
   const phonesContainer = document.getElementById("phones-container")
 
   phones.forEach(phone => {
+    
+    const {image,brand,phone_name,slug} = phone ;
   const div = document.createElement("div");
   div.classList = "card bg-base-100";
   div.innerHTML = `
   <figure class="px-10 pt-10">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src=${image}
       alt="Shoes"
       class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 class="card-title">${phone_name}</h2>
+    <p>${brand}</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
+      <button onclick = "showDatails('${slug}')" class="btn btn-primary">Show Datails</button>
     </div>
   </div>
 
@@ -52,24 +54,10 @@ const displayAllPhones = (phones) =>{
     
   }
   
-  
-    
-  
-
-    
-
-
-
-
-
 const handleShowAll = () => {
   loadAllPhones(true);
   
 };
-
-
-
-
 
 const handleSearch = () => {
   document.getElementById("spinner").style.display = "block";
@@ -83,5 +71,37 @@ const handleSearch = () => {
 
 };
 
+const loadDatails =async (slug) =>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+  const data = await res.json();
+  console.log(data.data);
+  
+    const modalContainer = document.getElementById("modal-container");
+    modalContainer.innerHTML = `
+   <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+    <h3 class="text-lg font-bold"></h3>
+    <p class="py-4">Press ESC key or click the button below to close</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+  `;
+
+    my_modal_5.showModal();
+
+  
+ 
+}
+
+
+
+
 loadAllPhones(false, "iphone");
+
+
 
---
+++ js/text.js
@@ -0,0 +1,35 @@
+// {
+//     "brand": "Apple ",
+//     "phone_name": "iPhone 13 mini",
+//     "slug": "apple_iphone_13_mini-11104",
+//     "image": "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg"
+// }
+// {
+//     "mainFeatures": {
+//         "storage": "128GB/256GB/512GB storage, no card slot",
+//         "displaySize": "6.7 inches, 109.8 cm2 (~87.4% screen-to-body ratio)",
+//         "chipSet": "Apple A14 Bionic (5 nm)",
+//         "memory": "128GB 6GB RAM, 256GB 6GB RAM, 512GB 6GB RAM",
+//         "sensors": [
+//             "Face ID",
+//             "accelerometer",
+//             "gyro",
+//             "proximity",
+//             "compass",
+//             "barometer"
+//         ]
+//     },
+//     "slug": "apple_iphone_12_pro_max-10237",
+//     "name": "iPhone 12 Pro Max",
+//     "releaseDate": "Released 2020, November 13",
+//     "brand": "Apple",
+//     "image": "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg",
+//     "others": {
+//         "WLAN": "Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot",
+//         "Bluetooth": "5.0, A2DP, LE",
+//         "GPS": "Yes, with A-GPS, GLONASS, GALILEO, QZSS",
+//         "NFC": "Yes",
+//         "Radio": "No",
+//         "USB": "Lightning, USB 2.0"
+//     }
+// }
